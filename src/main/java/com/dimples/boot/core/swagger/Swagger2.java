6e5d02a6551754c0c364463da12dff9909450d8d
dimples <1126834403@qq.com>
2019-11-07 15:05:42 +0800

    优化Swagger2配置

--- src/main/java/com/dimples/boot/core/swagger/Swagger2.java
+++ src/main/java/com/dimples/boot/core/swagger/Swagger2.java
@@ -8,6 +8,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -26,7 +27,7 @@ public class Swagger2 {
     @Bean
     public Docket createRestApi() {
         log.info("==================== 开启Swagger2配置 ====================");
-        log.info("==================== 访问网址：127.0.0.1:8001/swagger-ui.html ====================");
+        log.info("==================== 访问网址：IP:Port/swagger-ui.html ====================");
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
@@ -36,10 +37,12 @@ public class Swagger2 {
     }
 
     private ApiInfo apiInfo() {
+        Contact contact = new Contact("xxx公司", "http://139.9.220.139:8088", "1126834403@qq.com");
         return new ApiInfoBuilder()
                 .title("springboot利用swagger2构建api文档")
-                .description("简单优雅的restful风格，http://blog.csdn.net/forezp")
+                .description("项目名称")
                 .termsOfServiceUrl("127.0.0.1")
+                .contact(contact)
                 .version("1.0")
                 .build();
     }
