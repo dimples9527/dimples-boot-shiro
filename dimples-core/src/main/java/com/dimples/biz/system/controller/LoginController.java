3da3ae011a428f25b692289916d12ac9bb568700
dimples <1126834403@qq.com>
2019-11-14 17:28:01 +0800

    添加用户登录类

---
+++ dimples-core/src/main/java/com/dimples/biz/system/controller/LoginController.java
@@ -0,0 +1,32 @@
+package com.dimples.biz.system.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author zhongyj <1126834403@qq.com><br/>
+ * @date 2019/11/14
+ */
+@Slf4j
+@Api(value = "用户登录模块", tags = "用户登录模块")
+@RestController
+public class LoginController {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
