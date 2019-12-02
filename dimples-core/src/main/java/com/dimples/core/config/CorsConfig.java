f8554013b27ab09c62c048db57d04957e9df59c3
dimples <1126834403@qq.com>
2019-12-02 21:10:04 +0800

    跨域配置

--- dimples-core/src/main/java/com/dimples/core/config/CorsConfig.java
+++ dimples-core/src/main/java/com/dimples/core/config/CorsConfig.java
@@ -1,9 +1,8 @@
 package com.dimples.core.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * 跨域访问控制
@@ -12,26 +11,21 @@ import org.springframework.web.filter.CorsFilter;
  * @author zhongyj <1126834403@qq.com><br/>
  * @date 2019/11/18
  */
-public class CorsConfig {
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
 
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        // 允许任何域名使用
-        corsConfiguration.addAllowedOrigin("*");
-        // 允许任何头
-        corsConfiguration.addAllowedHeader("*");
-        // 允许任何方法（post、get等）
-        corsConfiguration.addAllowedMethod("*");
-        return corsConfiguration;
-    }
-
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        // 对接口配置跨域设置
-        source.registerCorsConfiguration("/**", buildConfig());
-        return new CorsFilter(source);
+    /**
+     * 页面跨域访问Controller过滤
+     *
+     * @param registry CorsRegistry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
     }
 
 }
