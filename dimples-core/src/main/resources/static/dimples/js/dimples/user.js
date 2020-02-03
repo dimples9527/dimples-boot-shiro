9340931c02a1a65a2e9836a1e92842d418002308
dimples <1126834403@qq.com>
2020-02-03 13:32:37 +0800

    更改表格渲染为js渲染

--- dimples-core/src/main/resources/static/dimples/js/dimples/user.js
+++ dimples-core/src/main/resources/static/dimples/js/dimples/user.js
@@ -5,11 +5,36 @@ let user = function () {
             type: "get",
             dataType: 'json',
             success: function (result) {
+                let userHtml = "";
                 if (result.code === 200) {
-                    console.log(result.data);
+                    let record = result.data.records;
+                    console.log("用户信息数据：" + record);
+                    for (let i = 0, len = record.length; i < len; i++) {
+                        let status = "无效";
+                        if (record[i].status === "1") {
+                            status = "有效";
+                        }
+                        userHtml += "<tr><td><label class='dimples-checkbox checkbox-primary'>" +
+                            "<input type='checkbox' name='ids[]' value=" + record[i].userId + "><span></span></label>" +
+                            "</td><td>" + record[i].userId + "</td><td>" + record[i].username + "</td><td>" + record[i].username + "</td>" +
+                            "<td>" + record[i].username + "</td><td>" + record[i].username + "</td><td>" + record[i].username + "</td><td>" +
+                            "<div class='layui-table-cell laytable-cell-1-0-6'>" +
+                            "<span class='layui-badge dimples-tag-green'>" + status + "</span></div></td>" +
+                            "<td>" + record[i].createDate + "</td><td><div class='btn-group'>" +
+                            "<a class='btn btn-xs btn-default' href='#' title='' " +
+                            "data-toggle='tooltip' data-original-title='查看'>" +
+                            "<i class='mdi mdi-eye-outline dimples-green'></i></a>" +
+                            "<a class='btn btn-xs btn-default' href='#' title='' " +
+                            "data-toggle='tooltip' data-original-title='编辑'>" +
+                            "<i class='mdi mdi-account-edit dimples-blue'></i></a>" +
+                            "<a class='btn btn-xs btn-default' href='#' title='' " +
+                            "data-toggle='tooltip' data-original-title='删除'>" +
+                            "<i class='mdi mdi-delete-variant dimples-red'></i></a></div></td></tr>";
+                    }
                 } else {
                     console.log(result.message);
                 }
+                $("#table-user-data").append(userHtml);
             }
         });
     };
