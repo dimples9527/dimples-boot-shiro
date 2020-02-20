3c5e3510f6c67d03afec6c11a21ad4828f3187a5
dimples <1126834403@qq.com>
2020-02-20 09:49:37 +0800

    修复页面角色显示

---
+++ dimples-core/src/main/resources/static/dimples/js/dimples-v1.js
@@ -0,0 +1,118 @@
+layui.use(['jquery', 'table'], function (exports) {
+    let $ = layui.jquery;
+    let layuiTable = layui.table;
+
+    let self = {};
+
+    // ----------------- 数据表封装 --------------------- //
+    self.table = {};
+    self.table.init = function (params) {
+        var defaultSetting = {
+            cellMinWidth: 80,
+            page: true,
+            skin: 'line',
+            limit: 10,
+            limits: [5, 10, 20, 30, 40, 100],
+            autoSort: false,
+            request: {
+                pageName: 'pageNum',
+                limitName: 'pageSize'
+            },
+            parseData: function (res) {
+                return {
+                    "code": res.code == 200 ? 0 : res.code,
+                    "count": res.data.total,
+                    "data": res.data.records
+                }
+            }
+        };
+        return layuiTable.render(
+            $.extend({}, defaultSetting, params)
+        );
+    };
+
+    // ----------------- model --------------------- //
+    // ----------------- 模态框类 --------------------- //
+    self.modal = {};
+    self.modal.base = function (msg, params) {
+        params = params || {};
+        params.titleIcoColor = params.titleIcoColor || '#5a8bff';
+        params.titleIco = params.titleIco || 'exclaimination';
+        params.title = params.title || [
+            '<i class="layui-icon layui-icon-' + params.titleIco + '" style="font-size:12px;background:' +
+            params.titleIcoColor +
+            ';display:inline-block;position:relative;top:-2px;height:21px;line-height:21px;text-align:center;width:21px;color:#fff;border-radius:50%;margin-right:12px;"></i>' +
+            params.titleValue,
+            'background:#fff;border:none;font-weight:bold;font-size:16px;color:#08132b;padding-top:10px;height:36px;line-height:46px;padding-bottom:0;'
+        ];
+        params = $.extend(
+            {
+                skin: 'layui-layer-admin-modal dimples-alert',
+                area: [windowWidth <= 750 ? '60%' : '400px'],
+                closeBtn: 0,
+                shadeClose: false
+            },
+            params
+        );
+        layer.alert(msg, params);
+    };
+
+    // 自定义方法
+    self.isUrl = function (str) {
+        return /^([hH][tT]{2}[pP]:\/\/|[hH][tT]{2}[pP][sS]:\/\/)(([A-Za-z0-9-~]+)\.)+([A-Za-z0-9-~\/])+$/.test(
+            str
+        )
+    };
+
+
+    // ----------------- 弹窗类 --------------------- //
+    self.alert = {};
+
+    function alertParams(msg, params) {
+        params.time = 3000;
+        params.shade = 0;
+        params.btn = null;
+        params.title = [
+            '<i class="layui-icon layui-icon-' +
+            params.titleIco +
+            '" style="font-size:12px;background:' +
+            params.titleIcoColor +
+            ';display:inline-block;font-weight:600;position:relative;top:-2px;height:21px;line-height:21px;text-align:center;width:21px;color:#fff;border-radius:50%;margin-right:12px;"></i>' +
+            (msg || '请填写提示信息'),
+            'background:#fff;border:none;font-weight:500;font-size:14px;color:#08132b;margin-bottom:-50px;padding:16px;height:45px;line-height:14px;padding-bottom:0;'
+        ];
+        params.offset = '40px';
+        params.area = [windowWidth <= 750 ? '80%' : '400px'];
+    }
+
+    self.alert.success = function (msg, params) {
+        params = params || {};
+        params.titleIco = 'ok';
+        params.titleIcoColor = '#30d180';
+        alertParams(msg, params);
+        self.modal.base('', params);
+    };
+    self.alert.warn = function (msg, params) {
+        params = params || {};
+        params.titleIco = 'exclaimination';
+        params.titleIcoColor = '#ffc107';
+        alertParams(msg, params);
+        self.modal.base('', params);
+    };
+    self.alert.error = function (msg, params) {
+        params = params || {};
+        params.titleIco = 'close';
+        params.titleIcoColor = '#ff5652';
+        alertParams(msg, params);
+        self.modal.base('', params);
+    };
+    self.alert.info = function (msg, params) {
+        params = params || {};
+        params.titleIco = 'infomation';
+        params.titleIcoColor = '#2db7f5';
+        alertParams(msg, params);
+        self.modal.base('', params);
+    };
+
+    exports('dimples', self);
+});
